fab835a7da241425774ff1d0fbc759bd468ba3cf
richardilemon <richardiupeju16@gmail.com>
2023-08-11 13:55:49 -0700

    Zero

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Calculate the result using the add function
+result = add(a, b)
+
+# Print the formatted result
+print("{} + {} = {}".format(a, b, result))
+
